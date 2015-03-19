5d62eb94b97033a323324a7d9cfc93a1fd2a2c1e
Martin Szymanski <martin@elfacht.com>
2015-03-19 15:00:59 +0100

    generated

--- _output/assets/js/main.js
+++ _output/assets/js/main.js
@@ -1,4 +1,4 @@
-/*! foobar-project-v0.1.0 2015-03-18 */
+/*! foobar-project-v0.1.0 2015-03-19 */
 
 var body=document.body,timer;window.addEventListener("scroll",function(){clearTimeout(timer),body.classList.contains("disable-hover")||body.classList.add("disable-hover"),timer=setTimeout(function(){body.classList.remove("disable-hover")},100)},!1);
 //# sourceMappingURL=main.js.map
